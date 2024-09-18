9a6d435560a70be992fafa59549d8e15f82b2f48
candido-mint <candidoluiz097@gmail.com>
2024-09-17 21:46:15 -0300

    feat(Inicio do projeto): Setup do projeto

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+export default {
+  content: [],
+  prefix: 'tw-',
+  theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+
+    extend: {},
+  },
+  plugins: [],
+}
+
