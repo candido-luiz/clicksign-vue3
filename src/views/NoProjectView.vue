f8e2ecb53f53248feae57c0c95539e464f1c5a3b
candido-mint <candidoluiz097@gmail.com>
2024-09-21 11:45:40 -0300

    feat(NoProjects): Ajusta tela de "sem projetos"

--- src/views/NoProjectView.vue
+++ src/views/NoProjectView.vue
@@ -8,13 +8,32 @@ import { useRouter } from 'vue-router';
 </script>
 
 <template>
-  <div class="tw-h-full tw-min-h-screen tw-max-h-screen">
-    <h1>Nenhum projeto</h1>
-    <p>Clique no botão abaixo para criar o primeiro e gerenciá-lo.</p>
-    <button @click="createNewProject" class="tw-p-4 tw-bg-blue-900" type="button">Novo projeto</button>
+  <div class="no-project-container">
+    <div class="no-projects">
+      <h1>Nenhum projeto</h1>
+      <p>Clique no botão abaixo para criar o primeiro e gerenciá-lo.</p>
+      <button @click="createNewProject" class="btn btn-primary" type="button">Novo projeto</button>
+    </div>
   </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.no-project-container {
+  height: calc(100vh - var(--clicksign-page-header-height));
+  padding: 42px 60px;
+}
+.no-projects {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+}
+h1 {
+  color: var(--clicksign-emphasis-text-color);
+  size: 24px;
+  margin-bottom: 24px;
+}
 </style>
