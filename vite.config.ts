f149d11b7273fb4f1b76c6b0f13bf93eb070e4cc
candido-mint <candidoluiz097@gmail.com>
2024-09-28 02:02:30 -0300

    chore(BaseURL): Configura corretamente a base url para o modo de dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/clicksign-vue3/",
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'legacy',
-        additionalData: `@import "@/assets/scss/global.scss";`,
+export default defineConfig(( { mode }) => {
+  return {
+    base: mode === 'development' ? "/" : "/clicksign-vue3/",
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'legacy',
+          additionalData: `@import "@/assets/scss/global.scss";`,
+        }
       }
     }
   }
