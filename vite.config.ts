24992d74d674cfb21db93869348e879368a53f02
candido-mint <candidoluiz097@gmail.com>
2024-09-28 02:16:15 -0300

    chore(BaseURL): Verifica ajuste do github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
-export default defineConfig(( { mode }) => {
+export default defineConfig(({ mode }: ConfigEnv) => {
   return {
     base: mode === 'development' ? "/" : "/clicksign-vue3/",
     plugins: [
