e5c5ccc7b407bd050c4f020ca238040b25b50ea6
candido-mint <candidoluiz097@gmail.com>
2024-09-30 00:57:26 -0300

    ci(Vitest): Ajusta configuracao do vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,8 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
-
-export default mergeConfig(
-  viteConfig as any,
+export default defineConfig(configEnv => mergeConfig(
+  viteConfig(configEnv),
   defineConfig({
     test: {
       environment: 'jsdom',
@@ -12,4 +11,4 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url))
     }
   })
-)
+))
